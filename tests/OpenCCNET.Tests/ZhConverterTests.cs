aeb492ec52b6ad5c693bdd5f79471045686b7810
Ed Ball <ed.ball@faithlife.com>
2023-10-18 13:59:58 -0700

    Add test.

---
+++ tests/OpenCCNET.Tests/ZhConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace OpenCCNET.Tests
+{
+	[TestFixture]
+	internal class ZhConverterTests
+	{
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			ZhConverter.Initialize();
+		}
+
+		[Test]
+		public void HansToFromTw()
+		{
+			const string simplified = "圣经研究的观点林林总总";
+			const string traditional = "聖經研究的觀點林林總總";
+			Assert.AreEqual(traditional, ZhConverter.HansToTW(simplified));
+			Assert.AreEqual(traditional, ZhConverter.HansToTW(traditional));
+			Assert.AreEqual(simplified, ZhConverter.TWToHans(traditional));
+			Assert.AreEqual(simplified, ZhConverter.TWToHans(simplified));
+		}
+	}
+}
